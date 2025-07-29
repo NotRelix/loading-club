4d0de908038be06161afe21073fc50d92fe72e32
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-07-29 21:59:40 +0800

    refactor: move validations to middlewares

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,64 +1,6 @@
 const { registerUser } = require("../db/query");
-const { body, validationResult } = require("express-validator");
-
-const emptyErr = "must not be empty";
-const alphaErr = "must only contain letters";
-const lengthErr = "must be between 1 and 10 characters";
-const passwordLengthErr = "must have at least 8 characters";
-
-const validateUser = [
-  body("firstName")
-    .trim()
-    .notEmpty()
-    .withMessage(`First name ${emptyErr}`)
-    .bail()
-    .isAlpha()
-    .withMessage(`First name ${alphaErr}`)
-    .bail()
-    .isLength({ min: 1, max: 10 })
-    .withMessage(`First name ${lengthErr}`),
-  body("lastName")
-    .trim()
-    .notEmpty()
-    .withMessage(`Last name ${emptyErr}`)
-    .bail()
-    .isAlpha()
-    .withMessage(`Last name ${alphaErr}`)
-    .bail()
-    .isLength({ min: 1, max: 10 })
-    .withMessage(`Last name ${lengthErr}`),
-  body("username")
-    .trim()
-    .notEmpty()
-    .withMessage(`Username ${emptyErr}`)
-    .bail()
-    .isAlpha()
-    .withMessage(`Username ${alphaErr}`)
-    .bail()
-    .isLength({ min: 1, max: 10 })
-    .withMessage(`Username ${lengthErr}`),
-  body("password")
-    .trim()
-    .notEmpty()
-    .withMessage(`Password ${emptyErr}`)
-    .bail()
-    .isLength({ min: 8 })
-    .withMessage(`Password ${passwordLengthErr}`),
-  body("confirmPassword")
-    .trim()
-    .notEmpty()
-    .withMessage(`Confirm Password ${emptyErr}`)
-    .bail()
-    .isLength({ min: 8 })
-    .withMessage(`Confirm Password ${passwordLengthErr}`)
-    .bail()
-    .custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error("Passwords do not match");
-      }
-      return true;
-    }),
-];
+const { validationResult } = require("express-validator");
+const { registerValidation } = require("../middlewares/validation");
 
 exports.registerUserGet = (req, res) => {
   res.render("register", {
@@ -68,7 +10,7 @@ exports.registerUserGet = (req, res) => {
 };
 
 exports.registerUserPost = [
-  validateUser,
+  registerValidation,
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
---
+++ middlewares/validation.js
@@ -0,0 +1,64 @@
+const { body } = require("express-validator");
+
+const emptyErr = "must not be empty";
+const alphaErr = "must only contain letters";
+const lengthErr = "must be between 1 and 10 characters";
+const passwordLengthErr = "must have at least 8 characters";
+
+const registerValidation = [
+  body("firstName")
+    .trim()
+    .notEmpty()
+    .withMessage(`First name ${emptyErr}`)
+    .bail()
+    .isAlpha()
+    .withMessage(`First name ${alphaErr}`)
+    .bail()
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`First name ${lengthErr}`),
+  body("lastName")
+    .trim()
+    .notEmpty()
+    .withMessage(`Last name ${emptyErr}`)
+    .bail()
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
+    .bail()
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Last name ${lengthErr}`),
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage(`Username ${emptyErr}`)
+    .bail()
+    .isAlpha()
+    .withMessage(`Username ${alphaErr}`)
+    .bail()
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Username ${lengthErr}`),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage(`Password ${emptyErr}`)
+    .bail()
+    .isLength({ min: 8 })
+    .withMessage(`Password ${passwordLengthErr}`),
+  body("confirmPassword")
+    .trim()
+    .notEmpty()
+    .withMessage(`Confirm Password ${emptyErr}`)
+    .bail()
+    .isLength({ min: 8 })
+    .withMessage(`Confirm Password ${passwordLengthErr}`)
+    .bail()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords do not match");
+      }
+      return true;
+    }),
+];
+
+module.exports = {
+  registerValidation,
+};
