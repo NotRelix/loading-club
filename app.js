121c81bb5088e6df5a93535f37b3885b0007d843
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-07-30 20:27:58 +0800

    feat: add basic layout for navbar

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set("view engine", "ejs");
 app.use(sessionMiddleware);
 app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", homeRouter);
 
