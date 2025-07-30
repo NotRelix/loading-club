9e37d7ceebd2a4a00be99dc6f17623b7db4ca22e
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-07-30 16:14:41 +0800

    feat: store sessions in Postgres with expiry

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("node:path");
 const homeRouter = require("./router/homeRouter");
 const session = require("express-session");
 const passport = require("passport");
+const pool = require("./db/pool");
+const pgSession = require("connect-pg-simple")(session);
 require("dotenv").config();
 require("./config/passport");
 
@@ -11,8 +13,21 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    pool: pool,
+    store: new pgSession({
+      pool: pool,
+      tableName: "session",
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
 app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 
