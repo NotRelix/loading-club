df1c360bdc4d763f270724f8075c6fc0cef79a5c
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-07-30 15:05:51 +0800

    feat: add logout GET route toend user sessions

--- router/homeRouter.js
+++ router/homeRouter.js
@@ -3,9 +3,11 @@ const homeRouter = Router();
 const homeController = require("../controllers/homeController");
 const registerRouter = require("./registerRouter");
 const loginRouter = require("./loginRouter");
+const logoutRouter = require("./logoutRouter");
 
 homeRouter.get("/", homeController.homeMessagesGet);
 homeRouter.use("/register", registerRouter);
 homeRouter.use("/login", loginRouter);
+homeRouter.use("/logout", logoutRouter);
 
 module.exports = homeRouter;
