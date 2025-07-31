531e9257636b081e99e91e6a127796f526f49ab3
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-07-31 21:58:31 +0800

    feat: add membership content

--- router/memberRouter.js
+++ router/memberRouter.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
 const memberRouter = Router();
 const memberController = require("../controllers/memberController");
+const { ensureAuthenticated } = require("../middlewares/auth");
 
-memberRouter.get("/", memberController.memberGet);
-memberRouter.post("/", memberController.memberPost);
+memberRouter.get("/", ensureAuthenticated, memberController.memberGet);
+memberRouter.post("/", ensureAuthenticated, memberController.memberPost);
 
 module.exports = memberRouter;
